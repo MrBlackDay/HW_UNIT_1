0e003fda1f3164614d88f344cbaa9a397fcefcee
Dima Lis <iselis777888999@gmail.com>
2023-09-21 23:13:54 +0300

    second commit testng fix aseertions

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -5,7 +5,7 @@ import static org.testng.Assert.assertEquals;
 public class CashbackHackServiceTest {
 
     @org.testng.annotations.Test
-    void limitValuesWhen1000ShouldBe0() {
+    public void limitValuesWhen1000ShouldBe0() {
 
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
@@ -16,46 +16,46 @@ public class CashbackHackServiceTest {
     }
 
     @org.testng.annotations.Test
-    void limitValuesWhen999ShouldBe1() {
+    public void limitValuesWhen999ShouldBe1() {
 
         CashbackHackService service = new CashbackHackService();
         int amount = 999;
         int actual = service.remain(amount);
         int expected = 1;
 
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
 
     @org.testng.annotations.Test
-    void limitValuesWhen1001ShouldBe999() {
+    public void limitValuesWhen1001ShouldBe999() {
 
         CashbackHackService service = new CashbackHackService();
         int amount = 1001;
         int actual = service.remain(amount);
         int expected = 999;
 
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
 
     @org.testng.annotations.Test
-    void equivalenceClassLess1000() {
+    public void equivalenceClassLess1000() {
 
         CashbackHackService service = new CashbackHackService();
         int amount = 888;
         int actual = service.remain(amount);
         int expected = 112;
 
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
 
     @org.testng.annotations.Test
-    void equivalenceClassMore1000() {
+    public void equivalenceClassMore1000() {
 
         CashbackHackService service = new CashbackHackService();
         int amount = 4200;
         int actual = service.remain(amount);
         int expected = 800;
 
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
 }
